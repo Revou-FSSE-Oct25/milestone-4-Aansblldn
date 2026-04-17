ae92c220dae22f588597fcf65685c068514d90e0
Aan Sabilladin <Aansabilladin@gmail.com>
2026-04-18 02:18:42 +0700

    add landing

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return '🚀 RevoBank API is Online! Visit <a href="/api-docs">/api-docs</a> for documentation.';
   }
 }
