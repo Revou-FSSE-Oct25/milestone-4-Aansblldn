7e4581692ebffcfa154b66a6ccaaa57554845bf3
Aan Sabilladin <Aansabilladin@gmail.com>
2026-04-18 02:28:31 +0700

    redirect to main

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,10 @@
 import { Controller, Get, Redirect } from '@nestjs/common';
-import { AppService } from './app.service';
+import { ApiExcludeEndpoint } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get()
+  @ApiExcludeEndpoint() 
   @Redirect('/api-docs', 301) 
   getHello() {
     
